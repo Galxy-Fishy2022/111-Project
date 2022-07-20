de871ff4960042b8fbbb78a4c6bd6f501ef3b1f0
Galxy-Fishy2022 <97571612+Galxy-Fishy2022@users.noreply.github.com>
2022-07-19 19:31:30 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,41 @@
 Webcam.set({
     width:350,
     height:300,
-    image_format : "png" ,
+    image_format: 'png',
     png_quality:90
 });
 
 camera = document.getElementById("camera");
 
-webcam.attach( '#camera' );
+Webcam.attach( '#camera');
 
 function take_snapshot()
 {
-    webcam.snap(function(data_uri) {
+    Webcam.snap(function(data_uri) {
         document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
     });
 }
-console.log("ml5_version", ml5_version);
 
-classifer = ml5.imageClassifer("put link" ,modleLoader);
+console.log('ml5 version:', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/c8CmUD2L4/model.json',modelLoaded);
+
+function modelLoaded() {
+    console.log('Model Loaded!');
+}
+
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
